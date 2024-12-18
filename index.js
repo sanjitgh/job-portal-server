d09d49f0b1d0b771edfc9a07ceb06db2de6e099c
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-18 16:32:47 +0600

    cooke function

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rwhf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,6 +35,19 @@ async function run() {
         // jobs related api
         const jobsCollection = client.db("jobportal").collection("jobs");
         const jobApplicationCollection = client.db("jobportal").collection("job_applications");
+
+        // job related APIs
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                })
+                .send({ success: true })
+        })
 
         // create jobs
         app.post("/jobs", async (req, res) => {
